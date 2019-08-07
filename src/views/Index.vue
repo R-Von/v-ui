936433a4b886a1f5fd2ce6a47007c57d0662d299
冯彤 <fengtong@p2peye.com>
2019-08-07 16:47:47 +0800

    feat(*):修改编译文件 再编译

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,12 @@
 <template>
     <div class="content">
+        <h2>V-UI</h2>
+
         <ul>
             <li><router-link to='/tab'>Tab切换</router-link></li>
+            <li>...</li>
+            <li>...</li>
+            <li>...</li>
             <li>...</li>
         </ul>
     </div>
